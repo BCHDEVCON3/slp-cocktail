c85e1fb20cf50bbe01a4a3ffef99b529aca66aeb
serh11p <git@serh11p.com>
2020-09-05 18:51:27 +0300

    Add BCH

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './fonts.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./fonts.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import "./bch-playground";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
