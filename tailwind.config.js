be8df2ed76d877c60c74d204084fc1af85c2afed
serh11p <git@serh11p.com>
2020-09-05 18:34:00 +0300

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [],
   theme: {
     extend: {
       colors: {
         orange: {
-          '500': '#f28705'
+          500: "#f28705",
         },
         blue: {
-          '500': '#05dbf2'
+          500: "#05dbf2",
         },
         red: {
-          '600': '#f20519'
+          600: "#f20519",
         },
         gray: {
-          '900': '#0a1926'
-        }
+          900: "#0a1926",
+        },
       },
       fontSize: {
-        '5xl': '2.75rem',
-        '8xl': '5.655rem'
-      }
-    }
+        "5xl": "2.75rem",
+        "8xl": "5.655rem",
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };
